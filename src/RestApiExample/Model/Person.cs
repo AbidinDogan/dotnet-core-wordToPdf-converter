741b33d5a03cf57dc758dafeecb1bf91f79db2cc
Abidin Dogan <abidin.dogan@superentegrator.com>
2021-06-18 15:17:10 +0300

    inital commit

---
+++ src/RestApiExample/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiExample.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
